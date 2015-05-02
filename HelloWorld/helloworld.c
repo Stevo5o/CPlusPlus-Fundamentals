81fae484c823b5e17c053ed61d983d0bea7fea40
Stevo5o <stevo.joc@gmail.com>
2015-05-02 14:08:40 +0100

    stdout, stderr

---
+++ HelloWorld/helloworld.c
@@ -0,0 +1,10 @@
+// helloworld.c
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+	// standard out
+	fprintf(stdout, "Hello, World!\n");
+	// standard error
+	fprintf(stderr, "Hello, World!\n");
+	return 0;
+}
--- HelloWorld/helloworld.cpp
+++
@@ -1,8 +0,0 @@
-// hello.cpp 
-#include <iostream>
-using namespace std;
-
-int main(int argc, char ** argv) {
-	cout << "Hello, World!\n";
-	return 0;
-}
