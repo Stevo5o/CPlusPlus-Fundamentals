564be6844b0ccc7ea0e0c163312e5ad250340c72
Stevo5o <stevo.joc@gmail.com>
2015-05-03 10:36:32 +0100

    C hello World with comments

---
+++ HelloWorld/helloworld.c
@@ -0,0 +1,10 @@
+// helloworld.c
+/* multi-line comment */
+#include <stdio.h> 
+
+// main is required not optional
+int main(int argc, char ** argv) {
+	// body of the function
+	printf("Hello, World!\n"); // all stement must have a semi-colon
+	return 0;
+}
