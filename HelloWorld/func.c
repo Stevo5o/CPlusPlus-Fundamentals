61ee979b49283424df6e14c310047bc1908da973
Stevo5o <stevo.joc@gmail.com>
2015-05-04 14:31:11 +0100

    C declaring vars & functions

--- HelloWorld/func.c
+++ HelloWorld/func.c
@@ -1,8 +1,17 @@
 // func.c 
 #include <stdio.h>
+#include "func.h" // declaration
 
-int var = 38;
+int main(int argc, char ** argv) {	
+	func();
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("i is %d\n", i);
+	}
+	return 0;
+}
 
 void func() {
 	printf("This is in the function\n");
-}
+} // definition
---
+++ HelloWorld/func.h
@@ -0,0 +1,8 @@
+// func.c
+
+#ifndef FUNC_H_
+#define FUNC_H_
+
+void func(); // declaration
+
+#endif /* FUNC_H_ */
