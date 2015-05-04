f4720a8b676c0cacd1d2e3c7fc9290678e31e855
Stevo5o <stevo.joc@gmail.com>
2015-05-04 12:23:28 +0100

    Delcare, define var add comments

--- HelloWorld/helloworld.c
+++ HelloWorld/helloworld.c
@@ -4,12 +4,13 @@
 
 // separation of concerns(SoC)
 int main(int argc, char ** argv) {
-	for (int i = 0; i < 5; i++) 
+	int count = 5; // declare and define var
+	char * s = "Hello, World!\n";
+	for (int i = 0; i < count; i++) 
 	{
 		printf("i is %d\n", i);
-		int x = printf("Hello, World!\n") * 5; // expressions in a statement
-		printf("printf returned %d\n", x); // printf value returns 70 statement
-
-	}
+		int x = printf(s) * 5; // expressions in a statement
+		printf("printf s string returned %d chars\n", x); // printf value returns 70 statement
+	} // end for loop
 	return 0; // excution was successful 0 = (EXIT_SUCCESS) 1 = (EXIT_FAILURE) 
 }
