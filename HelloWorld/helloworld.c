5662f72f2c6dd2ba636494d1fac2ccb88b4a575b
Stevo5o <stevo.joc@gmail.com>
2015-05-04 12:38:58 +0100

    Scope

--- HelloWorld/helloworld.c
+++ HelloWorld/helloworld.c
@@ -2,15 +2,17 @@
 // boolean  true = 1 /false = 0
 #include <stdio.h>
 
+int count = 5; 
+
 // separation of concerns(SoC)
-int main(int argc, char ** argv) {
-	int count = 5; // declare and define var
-	char * s = "Hello, World!\n";
-	for (int i = 0; i < count; i++) 
+int main(int argc, char ** argv) {	
+	for (int i = 0; i < count; i++)
 	{
 		printf("i is %d\n", i);
 		int x = printf(s) * 5; // expressions in a statement
 		printf("printf s string returned %d chars\n", x); // printf value returns 70 statement
 	} // end for loop
-	return 0; // excution was successful 0 = (EXIT_SUCCESS) 1 = (EXIT_FAILURE) 
-}
+	return 0;
+} // end main
+
+char * s = "Hello, World!\n"; // outside not visible to function
